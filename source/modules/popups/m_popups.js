d778087a86ac0ff5f779497aabb168cc78b882d5
Alexander Mischuk <mischuk.alexander@gmail.com>
2016-11-12 19:25:25 +0200

    Initial project

---
+++ source/modules/popups/m_popups.js
@@ -0,0 +1,1 @@
+console.log('Popups generated...');
---
+++ source/static/js/main.js
@@ -0,0 +1,31 @@
+$(function() {
+    $('a[href="#"]').click(function(e){ e.preventDefault(); });
+
+    /*! Polyfill to remove click delays on browsers with touch UIs */
+    FastClick.attach(document.body);
+
+
+    /*! IE10 viewport hack for Surface/desktop Windows 8 bug */
+    (function () {
+      'use strict';
+      if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement('style')
+        msViewportStyle.appendChild(
+          document.createTextNode(
+            '@-ms-viewport{width:auto!important}'
+          )
+        )
+        document.querySelector('head').appendChild(msViewportStyle)
+      }
+    })();
+
+    /*! Mask for form's input */
+    function inputMask() {
+      $(".mask-date").mask("99.99.9999",{placeholder:"__.__.____"});
+      $(".mask-year").mask("9999",{placeholder:""});
+      $(".mask-tel").mask("+7 (999) 999-99-99",{placeholder:"X"});
+    };
+    inputMask();
+
+    //=include modules.js
+});
